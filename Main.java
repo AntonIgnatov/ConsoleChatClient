b911edb0abd91d1c37ef5425eb1b99848bd9d681
AntonIgnatov <venzor@bigmir.net>
2018-05-29 15:09:04 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String login = "";
+        String password = "";
+        try {
+
+            boolean f = false;
+            while (!f) {
+                System.out.println("Enter your login: ");
+                login = scanner.nextLine();
+                System.out.println("Enter your password: ");
+                password = scanner.nextLine();
+                String resp = authorisation(login, password);
+                if (resp.startsWith("You connected as ")) {
+                    f = true;
+                }
+                System.out.println(resp);
+            }
+
+            Thread th = new Thread(new GetThread(login, password));
+            th.setDaemon(true);
+            th.start();
+
+
+            while (true) {
+//                System.out.println("Enter your message: ");
+                String text = scanner.nextLine();
+                if (text.isEmpty()) break;
+                Processor processor = new Processor(login, text);
+                processor.work();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public static String authorisation(String login, String password) throws IOException {
+        URL url = new URL(Utils.getURL() + "/authoris?login=" + login + "&password=" + password);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+
+        InputStream is = http.getInputStream();
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[10240];
+        int r;
+
+        do {
+            r = is.read(buf);
+            if (r > 0) bos.write(buf, 0, r);
+        } while (r != -1);
+
+
+        buf = bos.toByteArray();
+        return new String(buf, StandardCharsets.UTF_8);
+
+    }
+}
---
+++ Processor.java
@@ -0,0 +1,153 @@
+package com.company;
+
+import com.google.gson.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Processor {
+    private String text;
+    private String login;
+    private Gson gson = new GsonBuilder().create();
+
+    public Processor(String login, String text) {
+        this.login = login;
+        this.text = text;
+    }
+
+    public void work() throws IOException {
+        if (privateTo()) {
+            return;
+        } else if (toRoom()) {
+            return;
+        } else if (enterRoom()) {
+            return;
+        } else if (exitRoom()) {
+            return;
+        } else if (logOut()) {
+            return;
+        } else if (getStatus()) {
+            return;
+        } else if (help()) {
+            return;
+        } else {
+            Message m = new Message(login, text);
+            int res = m.send(Utils.getURL() + "/add");
+        }
+    }
+
+    public boolean privateTo() throws IOException {
+        if (this.text.startsWith("/PRIVATE ")) {
+            String to = this.text.substring(9, this.text.indexOf(' ', 9));
+            this.text = this.text.substring(8 + to.length() + 1);
+
+            Message m = new Message(login, text);
+            m.setTo(to);
+            int res = m.send(Utils.getURL() + "/add");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean toRoom() throws IOException {
+        if (this.text.startsWith("/ROOM ")) {
+            String room = this.text.substring(6, this.text.indexOf(' ', 6));
+            this.text = this.text.substring(6 + room.length() + 1);
+
+            Message m = new Message(login, text);
+            m.setRoom(room);
+            int res = m.send(Utils.getURL() + "/add");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean enterRoom() throws IOException {
+        if (this.text.startsWith("/ENTER ")) {
+            String room = this.text.substring(7);
+            String res = getData(Utils.getURL() + "/rooms?name=" + room + "&login=" + this.login);
+            System.out.println("You enter room " + room);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean exitRoom() throws IOException {
+        if (this.text.startsWith("/EXIT ")) {
+            String room = this.text.substring(6);
+            String res = getData(Utils.getURL() + "/rooms?name=" + room + "&login=" + this.login + "&exit=true");
+            System.out.println("You leave room " + room);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean logOut() throws IOException {
+        if (this.text.startsWith("/LEAVE")) {
+            String res = getData(Utils.getURL() + "/authoris?login="+ login + "&exit=true");
+            System.out.println("You leave chat(((");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean getStatus() {
+        if (this.text.startsWith("/INFO")) {
+            String url = Utils.getURL() + "/userstatus";
+            try {
+                String[] result = gson.fromJson(getData(url), String[].class);
+                for (String item : result) {
+                    System.out.println(item);
+
+                }
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+        }
+        return false;
+
+    }
+
+
+    private String getData(String sUrl) throws IOException {
+        String data = "";
+        URL url = new URL(sUrl);
+        HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+
+        if (connect.getContentLengthLong() != 0) {
+
+            try (InputStream is = connect.getInputStream(); InputStreamReader isr = new InputStreamReader(is, "utf-8")) {
+                StringBuilder sb = new StringBuilder();
+
+                BufferedReader br = new BufferedReader(isr);
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                data = sb.toString();
+            }
+        }
+
+        return data;
+    }
+
+    public boolean help(){
+        if (this.text.startsWith("/HELP")){
+            System.out.println("/PRIVATE username text -> send private message to username");
+            System.out.println("/ENTER roomname -> create chatroom or enter in existing");
+            System.out.println("/ROOM roomname text -> send message to chatroom roomname");
+            System.out.println("/EXIT roomname -> exit from chatroom");
+            System.out.println("/INFO -> get all users status");
+            System.out.println("/LEAVE -> disconnect from chat");
+            return true;
+        }
+        return false;
+    }
+}
